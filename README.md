389720b26a54335f8a1b4d75cc45eb1e52521c2a
Professor Carlos Muniz - Tokio Scools <professorcp@sapo.pt>
2025-07-24 17:54:51 +0000

    Hello World.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # first-web-app
+
+# Step 1: Install JDK 24
+
+sdk install java 24-open
+
+# Step 2: Move the content of the folder to CodeSpace
+
+# Step 3: Install Extension Pack for Java
+
+# Step 4: Open Java file and Run.
---
+++ src/main/java/com/cmuniz/firstweb/FirstwebApplication.java
@@ -0,0 +1,13 @@
+package com.cmuniz.firstweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstwebApplication.class, args);
+	}
+
+}
